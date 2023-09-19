9a7394f499c5eeb59487473509bd83b08053bef5
Carlo Pacheco <cpachecocerdas@gmail.com>
2023-09-18 19:00:04 -0600

    #"main.py" con formulas de LineaRecta Agregadas

---
+++ ProyectoProgramado1.py
@@ -0,0 +1,29 @@
+#Librerias a utilizar
+from math import * 
+import pandas as pd
+
+#Apertura de la fuente de datos.html
+def abrir_html():
+  ruta="C:\\Users\\cepc2\\OneDrive\\Desktop\\TEC\\2023 II Semestre\\Taller Programacion\\Primer Proyecto II Semestre\\PrimerProyectoTaller\\fuenteDeDatos.html"
+  datos = pd.read_html(ruta)[0]
+  print(datos)
+abrir_html()
+
+#Formulas
+#Modelo de Depreciacion por Linea Recta
+def LineaRecta(B,VS,n):
+    Lr = (B-VS)/n
+    return Lr
+
+#Modelo de Depreciacion por Linea Recta (Valor en libros)
+def LineaRectaVL(Lr, VS, n):
+    i = 1
+    while i<=n:
+        VL = VS - i*(Lr)
+        print("Valor en Libros ",i," = ",VL)
+        i = i + 1
+
+
+
+#Formar Tablas con DataFrame! 
+
